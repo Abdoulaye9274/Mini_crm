ee4a4ac6921394a98b4421e37b13c210511b51b3
Abdoulaye9274 <abdoulaye.doumbia1501@gmail.com>
2025-11-19 12:03:08 +0100

    Suppression fix_password.sql

--- fix_password.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM users WHERE login = 'admin';
-DELETE FROM users WHERE login = 'abdoulaye';
-INSERT INTO users (login, password, role) VALUES ('abdoulaye', '$2b$10$NLEMPX8vHQKVPV7R7z3bEeyBSuRtqNudWpudRiiVTuK0zx93A0f/O', 'admin');
